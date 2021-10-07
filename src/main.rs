d4b0e7b1cb938234bbc3e82460a5bf6eca091d5b
Shailesh Aanand <anaandshailu@gmail.com>
2021-10-07 11:28:30 +0530

    make a guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate rand;
+use rand::Rng;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number");
+    let secret = rand::thread_rng().gen_range(1, 1000);
+    loop {
+        println!("Enter your guess");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Fail");
+        let guess: i32 = guess.trim().parse().expect("Fail");
+        if guess == secret {
+            println!("Guessed Correctly");
+            break;
+        } else {
+            println!("Try Again!");
+            if guess > secret {
+                println!("Number is smaller");
+            } else {
+                println!("Number is bigger");
+            }
+        }
+    }
 }
